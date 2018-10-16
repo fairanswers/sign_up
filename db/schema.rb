37c9131c162589f19db4c10977673177b1a1ddcf
Joe Fair <joe@fairanswers.com>
2018-10-16 08:11:20 -0400

    Fixing all the odd files that creeped in from the .gitignore problems.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_235331) do
+ActiveRecord::Schema.define(version: 2018_10_16_115633) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_10_14_235331) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_time"
   end
 
   create_table "users", force: :cascade do |t|
