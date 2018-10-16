2a36bfe71eeb3b3f6fd853c2608a1a7fe0c9c4b8
Joe Fair <joe@fairanswers.com>
2018-10-16 08:21:04 -0400

    Typo in tasks migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_115633) do
+ActiveRecord::Schema.define(version: 2018_10_16_121405) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2018_10_16_115633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "start_time"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.boolean "taken"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_tasks_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
